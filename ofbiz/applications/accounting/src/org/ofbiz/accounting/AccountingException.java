178f83c6ca07b67772e385332f24e6574a578437
Samir Douichi <samirdouichi@gmail.com>
2013-01-31 13:27:31 -0500

    commit

--- ofbiz/applications/accounting/src/org/ofbiz/accounting/AccountingException.java
+++ ofbiz/applications/accounting/src/org/ofbiz/accounting/AccountingException.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  *******************************************************************************/
-
+ 
 package org.ofbiz.accounting;
 
 import org.ofbiz.service.GenericServiceException;
